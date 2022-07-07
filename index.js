7cb69051fecc3ac8f75a9cd1a8fd40861bd7ff6c
Daniel Guzman <guzdan@live.com>
2022-07-06 20:25:54 -0500

    commit tree 88541c7bb2251d1e7eaf4559b4c9e1b62fae5771

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// Add your code here
+function submitData(name, email) {
+    return fetch('http://localhost:3000/users', {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify({
+            "name": name,
+            "email": email
+        })
+    })
+        .then((response) => response.json())
+        .then((object) => {
+            const body = document.querySelector("body")
+            body.innerHTML = `${object.id}`
+        })
+        .catch((error) => {
+            const body = document.querySelector("body")
+            body.innerHTML += `${error.message}`
+        })
+}
+
+
+
+
+
+
